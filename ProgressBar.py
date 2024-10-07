324dda644ec9e291c404060fae7a9575b6200179
jMujunen <jMujunen@github.com>
2024-10-07 11:53:25 -0700

    Add support for executing module in a pipe

--- ProgressBar.py
+++ ProgressBar.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""A simple progress bar object"""
+"""A simple progress bar object."""
 
 import sys
 
@@ -9,12 +9,12 @@ from ExecutionTimer import ExecutionTimer
 class ProgressBar(ExecutionTimer):
     """
 
-    A simple progress bar object
+    A simple progress bar object.
 
     ### Attributes
     ----------
     `initial_value` : int
-        The initial value of the progress bar. Defaults to 100.
+        The initial value of the progress bar
 
     `current_value` : int
         The current value of the progress bar.
@@ -38,22 +38,24 @@ class ProgressBar(ExecutionTimer):
 
     """
 
-    def __init__(self, initial_value: int) -> None:
-        """Initializes a new instance of the class
+    def __init__(self, initial_value: int, print_on_exit=True) -> None:
+        """Initialize a new instance of the class.
 
         Parameters
         ----------
         - `initial_value` : int
             The initial value of the progress bar. Defaults to 100.
         """
-        super().__init__()
         self.initial_value = initial_value
-        self.value_ = 0
+        self.print_on_exit = print_on_exit
+        self._value = 0
         self.progress = 0
         self.errors = 0
+        if self.initial_value == -1:
+            sys.stdout.write("[%s] %i%%" % (" " * 40, 0))
 
     def increment(self, increment=1) -> None:
-        """Increments the current value of the progress bar by the given amount
+        """Increment the current value of the progress bar by the given amount.
 
         Parameters:
         ----------
@@ -64,45 +66,97 @@ class ProgressBar(ExecutionTimer):
             self.progress = self.value / self.initial_value * 100
         except ZeroDivisionError:
             self.errors += 1
-        output = f"[{self.progress:.1f}%]"
-        sys.stdout.write("\r" + output.ljust(int(self.progress / 2), "=") + "[100.0%]")
+        # output = f"[{self.progress:.1f}%]"
+        # sys.stdout.write("\r" + output.ljust(int(self.progress / 2), "=") + "[100.0%]")
+        # sys.stdout.flush()
+        sys.stdout.write("\r[%s] %i%%" % ("=" * (int(self.progress / 2)), self.progress))
         sys.stdout.flush()
+        if self.value == self.initial_value:
+            print()
 
     @property
     def value(self) -> int:
-        """Value getter property. Returns the current value"""
-        return int(self.value_)
+        """Value getter property. Returns the current value."""
+        return int(self._value)
 
     @value.setter
     def value(self, new_value: int) -> int:
-        """Value setter"""
-        self.value_ = new_value
+        """Value setter."""
+        self._value = new_value
         return self.value
 
     def __len__(self) -> int:
-        """Returns the length of the progress bar for use in a for loop"""
+        """Return the length of the progress bar for use in a for loop."""
         return self.initial_value
 
     def __int__(self) -> int:
-        """Int representation"""
+        """Int representation."""
         return int(self.value)
 
     def __str__(self) -> str:
-        """String representation"""
+        """Show object string representation."""
         return str(self.value)
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(initial_value={self.initial_value},\
-            current_value={self.value}, errors={self.errors}, progress={self.progress})"
+current_value={self.value}, errors={self.errors}, progress={self.progress})"
 
     def update(self) -> None:
-        """Force an update of the progress bar"""
+        """Force an update of the progress bar."""
         self.progress = self.value
-        output = f"[{self.progress:.1f}%]"
-        sys.stdout.write("\r" + output.ljust(int(self.progress / 2), "=") + "[100.0%]")
+        sys.stdout.write("\r[%s] %i%%" % ("=" * (50), 100))
         sys.stdout.flush()
+        print("\n")
 
     def complete(self) -> None:
-        """Sets the bar to complete manually"""
+        """Manually set the bar to complete."""
         self.value = self.initial_value
         self.update()
+
+
+def main(progressbar: ProgressBar, error: bool = False) -> None:
+    try:
+        for _ in iter(input, ""):
+            progressbar.increment()
+            progressbar.update()
+    except EOFError:
+        pass
+    finally:
+        progressbar.complete()
+
+
+if __name__ == "__main__":
+    import sys
+
+    initial_value = 0
+    if len(sys.argv) > 1:
+        initial_value = int(sys.argv[1])
+
+    # if not sys.stdin.isatty():
+    # # Read the total number of iterations from stdin
+    # try:
+    #     initial_value = int(sys.stdin.readline().strip())
+    #     print(initial_value)
+    # except ValueError:
+    #     print(
+    #         "Invalid input. Please provide a valid integer for the total number of iterations."
+    #     )
+    #     sys.exit(1)
+    # else:
+    # initial_value = 0
+    progress_bar = ProgressBar(initial_value)
+    print(initial_value)
+    if initial_value > 0:
+        with progress_bar as pb:
+            try:
+                for _ in iter(input, ""):
+                    pb.increment()
+            except KeyboardInterrupt:
+                pb.complete()
+                sys.exit(0)
+            except EOFError:
+                pass
+            finally:
+                sys.exit(0)
+    else:
+        sys.exit(1)
