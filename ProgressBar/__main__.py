4a08d41bf9c2885647943e87c17681d68381e8cf
jMujunen <jMujunen@github.com>
2024-11-05 12:32:53 -0800

    Decrease overhead improving performance by 40x in certain cases
    - Compile ProgressBar into cython with c data types, drastically
    	decreasing overhead
    - Modify support for pipes by moving the logic from
    	ProgressBar.py(x) -> __main__.py

--- ProgressBar/__main__.py
+++ ProgressBar/__main__.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+"""Support for using ProgressBar in pipes."""
+
+import sys
+
+from .ProgressBar import ProgressBar
+
+if __name__ == "__main__":
+    initial_value = 0
+    if len(sys.argv) > 1:
+        initial_value = int(sys.argv[1])
+
+    progress_bar = ProgressBar(initial_value)
+    print(initial_value)
+    if initial_value > 0:
+        with progress_bar as pb:
+            try:
+                for _ in iter(input, ""):
+                    pb.increment()
+            except KeyboardInterrupt:
+                pb.complete()
+                sys.exit(0)
+            except EOFError:
+                pass
+            finally:
+                sys.exit(0)
+    else:
+        sys.exit(1)
